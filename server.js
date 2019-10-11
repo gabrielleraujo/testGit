40d290616e916aa70b5d8fe5ce2d479563291b7f
Gabrielle Araújo <51082105+gabrielleraujo@users.noreply.github.com>
2019-10-11 12:19:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ exp.get("/", (req, res) => {
     res.status(200).send( { message: "Estou funcionando."} );
 })
 
+exp.get("/ola", (req, res) => {
+    res.status(200).send( { message: "Oi, eu sou a rota ola."} );
+})
+
 exp.listen(3001, () => {
     console.log("Api rodando na porta 3001.");
 })
